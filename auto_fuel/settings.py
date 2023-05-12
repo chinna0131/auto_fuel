f6d382fa9104b03bc3d69c04a003c9cdd4c77777
chinna0131 <durgaprasadp552@gmail.com>
2023-05-13 04:29:58 +0530

    data

--- auto_fuel/settings.py
+++ auto_fuel/settings.py
@@ -78,21 +78,29 @@ WSGI_APPLICATION = 'auto_fuel.wsgi.application'
 
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'auto_fuel',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'postgres',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#
+#         'NAME': 'auto_fuel',
+#
+#         'USER': 'postgres',
+#
+#         'PASSWORD': 'postgres',
+#
+#         'HOST': 'localhost',
+#
+#         'PORT': '5432',
+#
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
